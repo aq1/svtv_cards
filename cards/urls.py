e0d9d345479516e566e45026451a00648beb9a30
Vladimir Grechukhin <inmosh@protomail.ch>
2021-10-30 23:01:51 +0300

    Fix env

--- cards/urls.py
+++ cards/urls.py
@@ -6,5 +6,5 @@ from .views import ghost_webhook_view
 
 
 urlpatterns = [
-    path('/webhook/', ghost_webhook_view, name='ghost_webhook_view'),
+    path('webhook/', ghost_webhook_view, name='ghost_webhook_view'),
 ]
