bf0afd9cad9870dba9c0d30ed16056b7cb7c959f
Vladimir Grechukhin <inmosh@protomail.ch>
2021-10-31 02:00:31 +0300

    Update url

--- cards/urls.py
+++ cards/urls.py
@@ -6,5 +6,5 @@ from .views import post_updated_webhook
 
 
 urlpatterns = [
-    path('webhook/post_updated/', post_updated_webhook, name='ghost_webhook_view'),
+    path('post_updated/', post_updated_webhook, name='ghost_webhook_view'),
 ]
