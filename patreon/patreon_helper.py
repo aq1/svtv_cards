0beec0bebffb4b9f4755a9fadff8f6e3ee4c3fe1
Vladimir Grechukhin <inmosh@protomail.ch>
2022-01-25 16:53:16 +0300

    Add ping

--- patreon/patreon_helper.py
+++ patreon/patreon_helper.py
@@ -26,6 +26,11 @@ location.replace('https://svtv.org');
 '''
 
 
+@app.route("/ping")
+async def ping():
+    return quart.request.args.items(), 200
+
+
 @app.route("/api")
 async def not_main_route():
     user_id = quart.request.args.get("state")
