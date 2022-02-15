760befe15b97acc32b4214be5545826de5be2eeb
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-15 19:30:57 +0300

    Fix

--- test_constructor/src/test_constructor/fields/TestQuestionField.svelte
+++ test_constructor/src/test_constructor/fields/TestQuestionField.svelte
@@ -42,10 +42,6 @@
       </div>
     {/each}
   </div>
-  <div>
-    <Input bind:value={question.correct} placeholder="Текст правильного ответа"/>
-    <Input bind:value={question.wrong} placeholder="Текст непрвильного ответа"/>
-  </div>
   <Button text="+ ответ" callback={addAnswer}/>
 </div>
 
