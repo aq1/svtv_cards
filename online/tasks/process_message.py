603a50f630246f19895080cd3f2dbe233ec1845a
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-18 14:54:45 +0300

    Change template for online messages

--- online/tasks/process_message.py
+++ online/tasks/process_message.py
@@ -1,54 +1,15 @@
 import textwrap
-from collections import defaultdict
 
 from django.conf import settings
-from django.template.loader import render_to_string
-from django.utils import timezone
 from telegram import Bot
 
 from ghost.ghost_admin_request import create_post
-from ghost.ghost_admin_request import get_page
 from ghost.ghost_admin_request import get_post
-from ghost.ghost_admin_request import update_page
 from ghost.ghost_admin_request import update_post
 from online.models import OnlineMessage
 from project.celery import app
 
-
-@app.task
-def update_online_page():
-    messages = OnlineMessage.objects.filter(
-
-    ).order_by(
-        '-created_at',
-    )[:50]
-
-    sorted_messages = defaultdict(list)
-    for message in messages:
-        sorted_messages[message.created_at.date()].append(message)
-
-    sorted_messages = [[k, sorted_messages[k]] for k in reversed(sorted(sorted_messages.keys()))]
-    today = timezone.now().date()
-    yesterday = today - timezone.timedelta(days=1)
-
-    html = render_to_string(
-        'online/online_message_template.html',
-        context={
-            'online_messages': sorted_messages,
-            'today': today,
-            'yesterday': yesterday,
-        },
-    )
-
-    page = get_page(page_slug='online')
-    update_page(
-        page_id=page['id'],
-        page_updated_at=page['updated_at'],
-        data={
-            'html': html,
-            'authors': ['ruvalerydz@gmail.com'],
-        },
-    )
+from .update_online_page import update_online_page
 
 
 @app.task
