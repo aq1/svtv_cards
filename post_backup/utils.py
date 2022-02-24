437ab4e0a5b48b9c6cc325dcd8471888657334e3
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-25 00:07:24 +0400

    Fix logging

--- post_backup/utils.py
+++ post_backup/utils.py
@@ -2,6 +2,6 @@ from .models import PostBackup
 
 
 def log_post(post):
-    post_backup, _ = PostBackup.objects.get_or_create(id=post['id'])
+    post_backup, _ = PostBackup.objects.get_or_create(ghost_id=post['id'])
     post_backup.post = post
     post.save()
