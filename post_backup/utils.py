3ba03ea7522436a998d395adf125059563658351
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-25 00:15:55 +0400

    Add log

--- post_backup/utils.py
+++ post_backup/utils.py
@@ -2,6 +2,8 @@ from .models import PostBackup
 
 
 def log_post(post):
+    with open('p.txt', 'w') as f:
+        f.write(post)
     post_backup, _ = PostBackup.objects.get_or_create(ghost_id=post['id'])
     post_backup.post = post
     post.save()
