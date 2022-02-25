9d337e95f232bea1b073fc1736d121ed05e6cdbd
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-25 10:29:19 +0400

    Fix backup

--- post_backup/utils.py
+++ post_backup/utils.py
@@ -3,7 +3,7 @@ from .models import PostBackup
 
 def log_post(post):
     with open('p.txt', 'w') as f:
-        f.write(post)
+        f.write(str(post))
     post_backup, _ = PostBackup.objects.get_or_create(ghost_id=post['id'])
     post_backup.post = post
-    post.save()
+    post_backup.save()
