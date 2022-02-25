ef9926b7dd9a5aacdbca32e158c69b38ae77576b
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-25 12:09:51 +0400

    ouch

--- online/admin/online_message_admin.py
+++ online/admin/online_message_admin.py
@@ -5,4 +5,4 @@ from ..models import OnlineMessage
 
 @admin.register(OnlineMessage)
 class OnlineMessageAdmin(admin.ModelAdmin):
-    list_display = ['ghost_id', 'title']
+    list_display = ['ghost_id', 'message_service_id', 'title']
