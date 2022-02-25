2064717559589c0f94f077139862b90a4a54ec58
Vladimir Grechukhin <inmosh@protomail.ch>
2022-02-25 21:33:03 +0400

    Add history to post object

--- post_backup/admin/post_backup_admin.py
+++ post_backup/admin/post_backup_admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 
 from ..models import PostBackup
 
 
 @admin.register(PostBackup)
-class PostBackupAdmin(admin.ModelAdmin):
+class PostBackupAdmin(SimpleHistoryAdmin):
     list_display = ['ghost_id', 'updated_at', 'title']
