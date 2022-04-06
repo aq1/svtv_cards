e376086d0e9c0824d456b1212b453459ecbf8a66
Vladimir Grechukhin <inmosh@protomail.ch>
2022-04-06 22:34:51 +0300

    Add action

--- online/admin/online_message_admin.py
+++ online/admin/online_message_admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.core.checks import messages
 
 from ..models import OnlineMessage
+from ..tasks.upload_online_message_to_ghost import upload_online_message_to_ghost
 
 
 @admin.register(OnlineMessage)
 class OnlineMessageAdmin(admin.ModelAdmin):
     list_display = ['title', 'ghost_id', 'message_service_id']
+
+    actions = [
+        'upload_to_ghost',
+    ]
+
+    def upload_to_ghost(self, request, queryset):
+        list(map(upload_online_message_to_ghost.delay, queryset.values_list('id', flat=True)))
+        self.message_user(
+            request=request,
+            message=f'Создана задача на обновление постов',
+            level=messages.INFO,
+        )
