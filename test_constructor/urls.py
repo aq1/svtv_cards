e662b882126adf79d3199bd99ca53305e052de10
Vladimir Grechukhin <inmosh@protomail.ch>
2022-06-14 19:05:04 +0400

    Revert "Add vk view"
    This reverts commit 8aa2a697bfc4ec3306d91b3fb2f0fa6d581214d4.

--- test_constructor/urls.py
+++ test_constructor/urls.py
@@ -8,10 +8,8 @@ from .views.get_threads import get_threads
 from .views.save_test import save_test
 from .views.save_thread import save_thread
 from .views.upload_file import upload_file
-from .views.vk import vk
 
 urlpatterns = [
-    path('vk', vk, name='vk'),
     path('tests', TemplateView.as_view(template_name='test_constructor/index.html'), name='test_constructor'),
     path('threads', TemplateView.as_view(template_name='test_constructor/index.html'), name='thread_constructor'),
     path('upload-file/', upload_file, name='upload_file'),
